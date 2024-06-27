77b37b813b2c879678d13d6a8665899cdeff5161
lynnjaco <jacobs.lynnette@gmail.com>
2024-06-26 21:49:31 -0400

    adds the routes for each controller to the app file

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require("express");
 const app = express;
 
+const locationsController = require("./controllers/locations.controller.js");
+app.use("/locations", locationsController);
+
+const machinesController = require("./controllers/machines.controller.js");
+app.use("/machines", machinesController);
+
+const personsController = require("./controllers/persons.controller.js");
+app.use("/people", personsController);
+
+const plansController = require("./controllers/plans.controller.js");
+app.use("/plans", plansController);
+
+const specialEventsController = require("./controllers/special-events.controller.js");
+app.use("/special-events", specialEventsController);
+
 module.exports = app;

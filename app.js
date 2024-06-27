c5427f6c6de64047b048a431e9fe186a1015059d
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-06-27 10:56:31 -0400

    locations and persons routes working

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
+
+const locationsController = require("./controllers/locations.controller");
+app.use("/locations", locationsController);
+
+const personsController = require("./controllers/persons.controller");
+app.use("/persons", personsController);
+
+module.exports = app;

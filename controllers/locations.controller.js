8179cccb5ff16883b28a5f3622153a15e1545c60
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-26 22:01:14 -0400

    complete bonus for the people key

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -2,11 +2,29 @@
 const express = require("express");
 const locations = express.Router();
 const locationsArray = require("../models/location.model.js");
+const personsArray = require("../models/person.model.js");
+
+const bonusArray = locationsArray.map((address) => {
+    const sameZipPerson = []
+    for(let person of personsArray) {
+        if (address.zip === person.mainLocation) {
+            sameZipPerson.push(person)
+        }
+    } 
+    return {...address, "people":[...sameZipPerson]}
+})
+
 
 // Index
 locations.get("/", (req, res) => {
     res.json(locationsArray);
   });
+
+// Bonus Route
+
+locations.get("/people", (req, res) => {
+    res.json(bonusArray);
+})
 
 // Export
 module.exports = locations;

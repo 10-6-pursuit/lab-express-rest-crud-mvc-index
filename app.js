1ebd3ba3cbf30499a2da6bcb62690feaf0de5d9f
ROUSE-prog <stevenr2049@gmail.com>
2024-06-26 21:31:51 -0400

    Updated app.js to use the created controllers

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+const locationsController = require('./controllers/locations.controller');
+const personsController = require('./controllers/persons.controller');
+const plansController = require('./controllers/plans.controller');
+const machinesController = require('./controllers/machines.controller');
+const specialEventsController = require('./controllers/special-events.controller');
+
+app.use('/', locationsController);
+app.use('/', personsController);
+app.use('/', plansController);
+app.use('/', machinesController);
+app.use('/', specialEventsController);
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Sorry, no page found!');
 });
 
 module.exports = app;

5aabd43fa34318a1994af53ddfd5497b50571b36
incognito1025 <nkhanamart@gmail.com>
2024-06-27 00:14:45 -0400

    set up of models/

---
+++ models/location.model.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+      street: "45 Davis Street",
+      city: "Long Island City",
+      state: "NY",
+      zip: "11101",
+    },
+    {
+      street: "23 Broadway",
+      city: "New York",
+      state: "NY",
+      zip: "11011",
+    },
+  ];
+
+  /*
+
+export in two ways:
+module.exports = [];
+store it in a variable
+
+
+  */

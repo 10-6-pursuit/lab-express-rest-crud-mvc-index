3aa1c030d1ab9d09ab45461430321cbda7c14b6c
AntCannon <anthonycannonier@gmail.com>
2024-06-30 15:54:10 -0400

    add bonus GET /locations/people

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -6,4 +6,14 @@ locationsController.get('/', (req, res) => {
   res.json(locationModel)
 })
 
+locationsController.get('/people', (req, res) => {
+  const personModel = require('../models/person.model')
+  const locationsWithPeople = locationModel.map(location => {
+    location.members = personModel.filter(person => (
+      person.mainLocation === location.zip
+    ))
+  })
+  res.json(locationModel)
+})
+
 module.exports = locationsController
